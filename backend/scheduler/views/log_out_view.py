a3b9259b424edcc96ec7398b0e7ff1f0a6046271
Karim <karim.he@kcl.ac.uk>
2026-01-19 15:30:06 +0000

    Remove redundant views

--- backend/scheduler/views/log_out_view.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import logout
-from django.shortcuts import redirect
-
-
-def log_out(request):
-    """Log out the current user"""
-
-    logout(request)
-    return redirect('home')
