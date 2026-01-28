396daf9d7d5741352420273b73a0b882c2c2b968
Extreme Limbu <limbuextreme12@gmail.com>
2026-01-28 15:33:57 +0000

    Added wellbeing page

---
+++ scheduler/src/app/wellbeing/quote_block.js
@@ -0,0 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import styles from "./quote_block.module.css";
+
+export default function QuoteBlock() {
+  const [quote, setQuote] = useState("Loading...");
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("/api/wellbeing/center")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.quote) {
+          setQuote(data.quote);
+        } else {
+          setError("You can do this!");
+        }
+      })
+      .catch(() => setError("You can do this!"));
+  }, []);
+
+  if (error) return <p>{error}</p>;
+
+  return (
+    <quote-block>
+      <div className={styles["text"]}>
+        <svg className ={styles["corner"]} viewBox="0 0 65 62" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M35 3.5L65 6.5V62L0 0L35 3.5Z" fill="white"/>
+        </svg>
+        <p className="text"> "{quote}"</p>
+      </div>
+
+    </quote-block>
+  );
+}
