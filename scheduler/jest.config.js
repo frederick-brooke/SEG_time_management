ea077475f385351696bce9a77ea36556763eee1e
deetibabbar <deetibabbar06@gmail.com>
2026-02-19 15:10:11 +0000

    fix before test files

--- scheduler/jest.config.js
+++ scheduler/jest.config.js
@@ -1,27 +1,18 @@
-const nextJest = require('next/jest');
-
-const createJestConfig = nextJest({ dir: './' });
+const nextJest = require("next/jest");
+const createJestConfig = nextJest({ dir: "./" });
 
 const customJestConfig = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-};
-
-module.exports = async () => {
-  const config = await createJestConfig(customJestConfig)();
-  
-  config.transformIgnorePatterns = [
-    '/node_modules/(?!(jose|openid-client|next-auth|@fullcalendar|preact|@panva)/)',
-  ];
-
-  testEnvironment: "jsdom";
-
-  moduleNameMapper = {
+    "^@/(.*)$": "<rootDir>/$1",
+    "^components/(.*)$": "<rootDir>/src/components/$1",
     "\\.module\\.css$": "identity-obj-proxy",
-  };
-  
-  return config;
+  },
+
+  transformIgnorePatterns: [
+    "/node_modules/(?!(jose|openid-client|next-auth|@fullcalendar|preact|@panva)/)",
+  ],
 };
+
+module.exports = createJestConfig(customJestConfig);
