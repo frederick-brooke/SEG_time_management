ea5db3f47de7d9be0280fc6fbaf9ccb4afc049ba
deetibabbar <deetibabbar06@gmail.com>
2026-03-01 14:47:44 +0000

    quiz test file

--- scheduler/jest.config.js
+++ scheduler/jest.config.js
@@ -5,26 +5,12 @@ const customJestConfig = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '^components/(.*)$': '<rootDir>/src/components/$1',
-  },
-};
-
-module.exports = async () => {
-  const config = await createJestConfig(customJestConfig)();
-  
-  config.transformIgnorePatterns = [
-    '/node_modules/(?!(jose|openid-client|next-auth|@fullcalendar|preact|preact-render-to-string|@panva|bson|uuid)/)',
-  ];
-
-  return config;
-    "^@/(.*)$": "<rootDir>/$1",
+    "^@/(.*)$": "<rootDir>/src/$1",
     "^components/(.*)$": "<rootDir>/src/components/$1",
     "\\.module\\.css$": "identity-obj-proxy",
   },
-
   transformIgnorePatterns: [
-    "/node_modules/(?!(jose|openid-client|next-auth|@fullcalendar|preact|@panva)/)",
+    "/node_modules/(?!(jose|openid-client|next-auth|@fullcalendar|preact|preact-render-to-string|@panva|bson|uuid)/)",
   ],
 };
 
