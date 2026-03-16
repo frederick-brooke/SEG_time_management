8d60e89f18511c844845f538e5202a512eb01f8f
Aarushi Singh <aarushi.singh@kcl.ac.uk>
2026-03-16 15:28:58 +0000

    fix error

--- scheduler/src/lib/map/index.ts
+++ scheduler/src/lib/map/index.ts
@@ -2,3 +2,4 @@ export * from "./types";
 export * from "./constants";
 export * from "./utils";
 export { useGeolocation } from "./useGeolocation";
+export { calculateTravelTime as getTravelTime } from "@/src/lib/travel";
