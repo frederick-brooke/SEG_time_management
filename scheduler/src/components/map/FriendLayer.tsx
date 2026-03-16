8697c11fbb48e65dbc595a4f275d1260e32951a4
Aarushi Singh <aarushi.singh@kcl.ac.uk>
2026-03-16 14:39:56 +0000

    add Friends and Event layer components

---
+++ scheduler/src/components/map/FriendLayer.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { Marker, Popup } from "react-leaflet";
+import { Icon } from "leaflet";
+import { Friend, USER_ICON_URL, FRIEND_ICON_URL } from "@/src/lib/map";
+
+interface FriendLayerProps {
+  friends: Friend[];
+  userLocation: [number, number] | null;
+}
+
+const userIcon = new Icon({ iconUrl: USER_ICON_URL, iconSize: [38, 38] });
+const friendIcon = new Icon({ iconUrl: FRIEND_ICON_URL, iconSize: [32, 32] });
+
+/**
+ * Renders the current user's position plus all friends as Leaflet markers.
+ */
+export function FriendLayer({ friends, userLocation }: FriendLayerProps) {
+  return (
+    <>
+      {userLocation && (
+        <Marker position={userLocation} icon={userIcon}>
+          <Popup>
+            <div className="text-center">
+              <strong>You are here!</strong>
+            </div>
+          </Popup>
+        </Marker>
+      )}
+
+      {friends.map((friend) => {
+        if (!friend.location) return null;
+        return (
+          <Marker
+            key={friend.id}
+            position={[friend.location.lat, friend.location.lng]}
+            icon={friendIcon}
+          >
+            <Popup>
+              <div className="text-center">
+                {friend.pfp && (
+                  <img
+                    src={friend.pfp}
+                    alt={friend.name}
+                    className="w-12 h-12 rounded-full mx-auto mb-2 border-2 border-white shadow"
+                  />
+                )}
+                <strong>{friend.name}</strong>
+                <br />
+                <span className="text-sm text-gray-600">
+                  {friend.city && friend.country
+                    ? `${friend.city}, ${friend.country}`
+                    : friend.username}
+                </span>
+              </div>
+            </Popup>
+          </Marker>
+        );
+      })}
+    </>
+  );
+}
