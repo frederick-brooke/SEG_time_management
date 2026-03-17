d8791a88aff15b4057bee28cade9d7a541397b50
Joanna Jayachandran <jjoannarachel@gmail.com>
2026-03-17 11:58:50 +0000

    Remove header text from task page

--- scheduler/src/app/(pages)/tasks/page.jsx
+++ scheduler/src/app/(pages)/tasks/page.jsx
@@ -38,14 +38,6 @@ export default function TasksPage() {
 
   return (
     <div className="flex flex-col gap-4 p-4 md:p-6">
-      {/* Page Header */}
-      <div className="flex flex-col gap-2">
-        <h1 className="text-3xl font-bold">Tasks</h1>
-        <p className="text-muted-foreground">
-          Manage your tasks and track your progress
-        </p>
-      </div>
-
       {/* To-Do List Component */}
       <ToDoList userId={session.user.id} exams={exams} />
     </div>
