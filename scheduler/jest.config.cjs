7a1bb67a17180cae637dd893719af488f20ee396
Aarushi Singh <aarushi.singh@kcl.ac.uk>
2026-03-23 09:58:15 +0000

    fix in test coverage

--- scheduler/jest.config.cjs
+++ scheduler/jest.config.cjs
@@ -4,17 +4,22 @@ const createJestConfig = nextJest({ dir: "./" });
 const customJestConfig = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  forceExit: true,
+  maxWorkers: 1,
+  coveragePathIgnorePatterns: [
+    "src/components/friend-map/map.tsx",
+  ],
   moduleNameMapper: {
     "^@/src/(.*)$": "<rootDir>/src/$1",
     "^@/(.*)$": "<rootDir>/src/$1",
     "^components/(.*)$": "<rootDir>/src/components/$1",
     "\\.module\\.css$": "identity-obj-proxy",
     "^hooks/(.*)$": "<rootDir>/src/hooks/$1",
-    "^@/(.*)$": "<rootDir>/src/$1",
+    "^leaflet$": "<rootDir>/__mocks__/leaflet.js",
   },
   transformIgnorePatterns: [
     "/node_modules/(?!(jose|openid-client|next-auth|@fullcalendar|preact|preact-render-to-string|@panva|bson|uuid)/)",
   ],
 };
 
-module.exports = createJestConfig(customJestConfig);
+module.exports = createJestConfig(customJestConfig);  
