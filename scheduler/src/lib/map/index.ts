83d1572beecf38399757d208456829e103e4ef77
Freddie Brooke <freddiegbrooke@gmail.com>
2026-03-26 19:14:36 +0000

    feat: implement useFriends hook and integrate with MapView component

--- scheduler/src/lib/map/index.ts
+++ scheduler/src/lib/map/index.ts
@@ -2,4 +2,5 @@ export * from "./types";
 export * from "./constants";
 export * from "./utils";
 export { useGeolocation } from "./useGeolocation";
+export { useFriends } from "./useFriends";
 export { calculateTravelTime as getTravelTime } from "@/lib/travel";
