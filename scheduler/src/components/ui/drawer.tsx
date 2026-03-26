03742cf69e9e7de9dcc4ae33bb2b127dd56eac17
Aarushi Singh <aarushi.singh@kcl.ac.uk>
2026-03-26 19:52:27 +0000

    remove comment

--- scheduler/src/components/ui/drawer.tsx
+++ scheduler/src/components/ui/drawer.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import { Drawer as DrawerPrimitive } from "vaul"
 
-import { cn } from "@/lib/utils" // Added @ prefix as is standard in most setups
+import { cn } from "@/lib/utils" 
 
 function Drawer({
   ...props
