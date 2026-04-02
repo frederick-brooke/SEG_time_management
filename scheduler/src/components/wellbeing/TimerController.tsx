ed3cda2778cae2c57d78e8fcddd5a776eafb408f
Aarushi Singh <aarushi.singh@kcl.ac.uk>
2026-04-02 12:40:24 +0100

    fix import errors

--- scheduler/src/components/wellbeing/TimerController.tsx
+++ scheduler/src/components/wellbeing/TimerController.tsx
@@ -1,13 +1,8 @@
 "use client";
 
 import { useState, useRef, useEffect, useCallback } from "react";
-<<<<<<< HEAD
-import Timer from "@/components/wellbeing/timer";
-import ReminderModal from "@/components/ui/reminderModal";
-=======
 import Timer from "@/components/wellbeing/Timer";
 import ReminderModal from "@/components/ui/ReminderModal";
->>>>>>> origin
 import { useUI } from "@/context/UIContext";
 
 /**
