1af6a4a238a0b060f29a5720ec8a73d6734f0546
Karim <karim.he@kcl.ac.uk>
2026-04-09 18:37:17 +0100

    Fix calendarEvents test

--- scheduler/src/components/calendar/__tests__/CalendarEvents.test.tsx
+++ scheduler/src/components/calendar/__tests__/CalendarEvents.test.tsx
@@ -92,7 +92,7 @@ describe("CalendarEvents", () => {
 
 		render(<CalendarEvents />);
 
-		const rendered = await screen.findAllByText(/Event/);
+		const rendered = await screen.findAllByText(/^Event \d+$/);
 		expect(rendered.length).toBeLessThanOrEqual(5);
 	});
 
